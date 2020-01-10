e6d2c6cf37ef3d7c4b8c708fed12f7e8faca3f0e
boostinwrx <tony.calablava@gmail.com>
2020-01-09 22:00:25 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,35 @@
-// add code snippets from README
+
+
+let state
+
+ reducer = (state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1}
+        default:
+            return state;
+    }
+
+ }
+
+dispatch = action => {
+    state = reducer(state,action)
+    render()
+}
+
+render = () => {
+    let container = document.getElementById('container')
+container.textContent = state.count
+
+}
+dispatch({type:'@@INIT'})
+
+let button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    dispatch({type: 'INCREASE_COUNT'})
+})
+
+// reducer(state, {type: 'INCREASE_COUNT'});
+
+
