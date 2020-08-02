e6428350e8088bcac377f9b9d04f9f0695d035c2
ocalvario <oswald.calvario@gmail.com>
2020-08-01 22:54:41 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,24 @@
-// add code snippets from README
+let state;
+
+function reducer(state = {count: 0}, action) {
+    switch(action.type){
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1 };
+        case 'DECREASE_COUNT':
+            return {count: state.count - 1 };
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render();
+}
+
+function render() {
+    document.getElementById('container').innerText = state.count;
+}
+
+document.getElementById('button').addEventListener('click', e => dispatch({type: 'INCREASE_COUNT'}));
+dispatch({count: '@@INIT'});
