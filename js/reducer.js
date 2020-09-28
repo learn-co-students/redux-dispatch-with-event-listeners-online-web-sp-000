c52226d390ab23557605591f5e165e5dce3f1e8e
github username <github email address>
2020-09-28 18:52:14 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,34 @@
-// add code snippets from README
+let state; //dispatching an initial action
+ 
+function reducer(state = {count: 0}, action){ //default argument
+  switch (action.type) { //pass an action and previous state to our reducer 
+    case 'INCREASE_COUNT': //increase our state 
+      return {count: state.count + 1}
+    default:
+      return state; //return new state 
+  }
+}
+ 
+//wrap the execution of our reducer in a function we call dispatch
+function dispatch(action){
+  state = reducer(state, action)
+  render();
+}
+ 
+//render function displays our state
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+ 
+//default state and then call render 
+dispatch({type: '@@INIT'})
+
+//integrating dispatch with a user event 
+//want an action to be dispatched each time a user clicks 
+//javascript event listener:
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
