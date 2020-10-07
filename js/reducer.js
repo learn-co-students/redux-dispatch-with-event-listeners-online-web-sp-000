31c54240c37b8f2495a2cb2ae583c2caeddc8cb7
Cole Rau <colevrau@gmail.com>
2020-10-06 17:24:31 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,30 @@
 // add code snippets from README
+
+let state;
+
+function reducer(state = {count: 0}, action) {
+    switch(action.type) {
+        default: 
+            return state
+        case "INCREASE_COUNT": 
+            return state = {count: state.count + 1}
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+    render()
+}
+
+function render() {
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+dispatch({type: "yeet"})
+
+let button = document.getElementById('button');
+
+button.addEventListener("click", () => {
+    dispatch({type: "INCREASE_COUNT"})
+})
